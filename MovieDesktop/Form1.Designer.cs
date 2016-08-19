cba2cae37b52b5da03765b60f07aa055fd064e2c
demoにゃん～ <dave_kura@outlook.com>
2016-08-19 17:43:52 +0800

    Create Project

---
+++ MovieDesktop/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MovieDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.上一桌面ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.下一桌面ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.暂停ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.停止ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.voiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.AllowDrop = true;
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Margin = new System.Windows.Forms.Padding(0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(377, 289);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.MouseDownEvent += new AxWMPLib._WMPOCXEvents_MouseDownEventHandler(this.axWindowsMediaPlayer1_MouseDownEvent);
+            this.axWindowsMediaPlayer1.MouseMoveEvent += new AxWMPLib._WMPOCXEvents_MouseMoveEventHandler(this.axWindowsMediaPlayer1_MouseMoveEvent);
+            this.axWindowsMediaPlayer1.MouseUpEvent += new AxWMPLib._WMPOCXEvents_MouseUpEventHandler(this.axWindowsMediaPlayer1_MouseUpEvent);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "梦幻桌面V0.0.0.1Bate--voidint开发";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.上一桌面ToolStripMenuItem,
+            this.下一桌面ToolStripMenuItem,
+            this.开始ToolStripMenuItem,
+            this.暂停ToolStripMenuItem,
+            this.停止ToolStripMenuItem,
+            this.voiceToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 158);
+            // 
+            // 上一桌面ToolStripMenuItem
+            // 
+            this.上一桌面ToolStripMenuItem.Name = "上一桌面ToolStripMenuItem";
+            this.上一桌面ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.上一桌面ToolStripMenuItem.Text = "上一桌面";
+            this.上一桌面ToolStripMenuItem.Click += new System.EventHandler(this.上一桌面ToolStripMenuItem_Click);
+            // 
+            // 下一桌面ToolStripMenuItem
+            // 
+            this.下一桌面ToolStripMenuItem.Name = "下一桌面ToolStripMenuItem";
+            this.下一桌面ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.下一桌面ToolStripMenuItem.Text = "下一桌面";
+            this.下一桌面ToolStripMenuItem.Click += new System.EventHandler(this.下一桌面ToolStripMenuItem_Click);
+            // 
+            // 开始ToolStripMenuItem
+            // 
+            this.开始ToolStripMenuItem.Enabled = false;
+            this.开始ToolStripMenuItem.Name = "开始ToolStripMenuItem";
+            this.开始ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.开始ToolStripMenuItem.Text = "开始";
+            this.开始ToolStripMenuItem.Click += new System.EventHandler(this.开始ToolStripMenuItem_Click);
+            // 
+            // 暂停ToolStripMenuItem
+            // 
+            this.暂停ToolStripMenuItem.Name = "暂停ToolStripMenuItem";
+            this.暂停ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.暂停ToolStripMenuItem.Text = "暂停";
+            this.暂停ToolStripMenuItem.Click += new System.EventHandler(this.暂停ToolStripMenuItem_Click);
+            // 
+            // 停止ToolStripMenuItem
+            // 
+            this.停止ToolStripMenuItem.Name = "停止ToolStripMenuItem";
+            this.停止ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.停止ToolStripMenuItem.Text = "停止";
+            this.停止ToolStripMenuItem.Click += new System.EventHandler(this.停止ToolStripMenuItem_Click);
+            // 
+            // voiceToolStripMenuItem
+            // 
+            this.voiceToolStripMenuItem.Name = "voiceToolStripMenuItem";
+            this.voiceToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.voiceToolStripMenuItem.Text = "静音";
+            this.voiceToolStripMenuItem.Click += new System.EventHandler(this.voiceToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 289);
+            this.ControlBox = false;
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 下一桌面ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 开始ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 停止ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem voiceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 上一桌面ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 暂停ToolStripMenuItem;
+    }
+}
+
---
+++ MovieDesktop/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace MovieDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.dll")]
+        public static extern long SetParent(long hWndChild, long hWndNewParent);
+        [DllImport("user32.dll")]
+        public static extern long SetWindowPos(long hwnd, long hWndInsertAfter, long x, long y, long cx, long cy, long wFlags);
+        [DllImport("user32.dll")]
+        public static extern long SetLayeredWindowAttributes(long hwnd, long crKey, byte bAlpha, long dwFlags);
+        [DllImport("user32.dll")]
+        public static extern long GetWindowLong(long hwnd, long nIndex);
+        [DllImport("user32.dll")]
+        public static extern long SetWindowLong(long hwnd, long nIndex, long dwNewLong);
+        [DllImport("user32.dll")]
+        public static extern long FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern long FindWindowEx(long hWnd1, long hWnd2, string lpsz1, string lpsz2);
+        [DllImport("user32.dll")]
+        public static extern long SetCapture(long hwnd);
+        [DllImport("user32.dll")]
+        public static extern long PostMessage(long hWnd, uint Msg, long wParam, long lParam);
+        [DllImport("user32.dll")]
+        public static extern long SendMessage(long hwnd, long wMsg, long wParam, long lParam);
+
+        private const long WS_EX_LAYERED = 0x80000;
+        private const long GWL_EXSTYLE = -20;
+        private const long LWA_ALPHA = 0x2;
+        private const long LWA_COLORKEY = 0x1;
+        private const long WS_EX_TRANSPARENT = 0x20;
+
+
+        public const uint WM_LBUTTONDOWN = 0x0201;
+        public const uint WM_LBUTTONUP = 0x0202;
+        public const uint WM_RBUTTONDOWN = 0x0204;
+        public const uint WM_RBUTTONUP = 0x0205;
+        public const uint WM_MOUSEMOVE = 0x0200;
+
+        public const long WM_SETCURSOR = 0x0020;
+        public const long WM_PARENTNOTIFY = 0x0210;
+
+
+
+        public long h1, h2, h3, rba;
+        public int typ = -1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.uiMode = "None";
+            axWindowsMediaPlayer1.enableContextMenu = false;
+            axWindowsMediaPlayer1.settings.autoStart = false;
+            //AxWindowsMediaPlayer1.settings.playCount = -1;
+            axWindowsMediaPlayer1.settings.setMode("loop", true);
+            axWindowsMediaPlayer1.stretchToFit = true;
+            axWindowsMediaPlayer1.settings.volume = 100;
+            Size s = Screen.PrimaryScreen.Bounds.Size;
+            //s.Width += 10;
+            this.Size = s;
+            axWindowsMediaPlayer1.Size = s;
+            axWindowsMediaPlayer1.URL = "F:\\Programtempfile\\1.wpl";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            h1 = 0;
+            int cont = 0;
+            while (h2 == 0)
+            {
+                if (cont >= 100)
+                {
+                    MessageBox.Show("未找到桌面！", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    h2 = 1;
+                    Application.Exit();
+                }
+                cont++;
+                h1 = FindWindowEx(0, h1, "WorkerW", null); //获得第一个WorkerW类的窗口
+                h2 = FindWindowEx(h1, 0, "SHELLDLL_DefView", null);
+                //Debug.WriteLine(h1 & " " & h2)
+                if (h2 == 0)
+                    continue;
+                h3 = FindWindowEx(h2, 0, null, "folderview");
+            }
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            SetParent(h2, this.Handle.ToInt64());
+
+            long rtn;
+            rtn = GetWindowLong(h2, GWL_EXSTYLE);
+            rba = rtn;
+            //rtn = rtn | WS_EX_LAYERED | WS_EX_TRANSPARENT;
+            rtn = rtn | WS_EX_LAYERED;
+            SetWindowLong(h2, GWL_EXSTYLE, rtn);
+            SetLayeredWindowAttributes(h2, 0, 10, LWA_COLORKEY);
+            //SetCapture(h2);
+            //SetWindowPos(h1, 1, 30, 30, s.Width, s.Height, 0);
+            SetParent(this.Handle.ToInt64(), h1);
+
+            SetWindowPos(this.Handle.ToInt64(), 1, 0, 0, 0, 0, 0);
+            //this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void 下一桌面ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.next();
+        }
+
+        private void voiceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (voiceToolStripMenuItem.Text == "静音")
+            {
+                axWindowsMediaPlayer1.settings.mute = true;
+                voiceToolStripMenuItem.Text = "恢复音量";
+            }
+            else
+            {
+                voiceToolStripMenuItem.Text = "静音";
+                axWindowsMediaPlayer1.settings.mute = false;
+            }
+        }
+
+        private void 停止ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+            SetLayeredWindowAttributes(h2, 0, 255, LWA_COLORKEY);
+            SetWindowLong(h2, GWL_EXSTYLE, rba);
+            SetParent(h2, h1);
+            开始ToolStripMenuItem.Enabled = true;
+            停止ToolStripMenuItem.Enabled = false;
+            下一桌面ToolStripMenuItem.Enabled = false;
+            上一桌面ToolStripMenuItem.Enabled = false;
+            暂停ToolStripMenuItem.Enabled = false;
+            this.Hide();
+        }
+
+        private void 开始ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            SetParent(h2, this.Handle.ToInt64());
+            long rtn;
+            rtn = GetWindowLong(h2, GWL_EXSTYLE);
+            //rba = rtn;
+            rtn = rtn | WS_EX_LAYERED;
+            SetWindowLong(h2, GWL_EXSTYLE, rtn);
+            SetLayeredWindowAttributes(h2, 0, 10, LWA_COLORKEY);
+            //SetParent(Me.Handle, h1);
+            //SetWindowPos(this.Handle.ToInt64(), 1, 0, 0, 0, 0, 0);
+            停止ToolStripMenuItem.Enabled = true;
+            开始ToolStripMenuItem.Enabled = false;
+            下一桌面ToolStripMenuItem.Enabled = true;
+            上一桌面ToolStripMenuItem.Enabled = true;
+            暂停ToolStripMenuItem.Enabled = true;
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetLayeredWindowAttributes(h2, 0, 255, LWA_COLORKEY);
+            SetWindowLong(h2, GWL_EXSTYLE, rba);
+            SetParent(h2, h1);
+            this.Close();
+        }
+
+        private void 上一桌面ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.previous();
+        }
+
+        private void axWindowsMediaPlayer1_MouseUpEvent(object sender, AxWMPLib._WMPOCXEvents_MouseUpEvent e)
+        {
+            if (e.nButton == 1)
+            {
+                PostMessage(h2, WM_LBUTTONUP, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                typ = 1;
+                //PostMessage(h2, WM_LBUTTONUP, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                //SendMessage(h2, WM_PARENTNOTIFY, WM_LBUTTONUP, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                //SendMessage(h2, WM_SETCURSOR, h3, (WM_LBUTTONUP<<16) |0x0001 );
+            }
+            else
+            {
+                PostMessage(h2, WM_RBUTTONUP, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                typ = 2;
+                //PostMessage(h2, WM_RBUTTONUP, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                //SendMessage(h2, WM_PARENTNOTIFY, WM_RBUTTONUP, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                //SendMessage(h2, WM_SETCURSOR, h3, (WM_RBUTTONUP<<16) |0x0001);
+            }
+        }
+
+        private void axWindowsMediaPlayer1_MouseDownEvent(object sender, AxWMPLib._WMPOCXEvents_MouseDownEvent e)
+        {
+            //Debug.WriteLine(e.fX.ToString());
+            if (e.nButton == 1)
+            {
+                PostMessage(h2, WM_LBUTTONDOWN, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                typ = 3;
+                //PostMessage(h2, WM_LBUTTONDOWN, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                //SendMessage(h2, WM_PARENTNOTIFY, WM_LBUTTONDOWN, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                //SendMessage(h2, WM_SETCURSOR, h3, (WM_LBUTTONDOWN<<16) |0x0001);
+            }
+            else
+            {
+                PostMessage(h2, WM_RBUTTONDOWN, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                typ = 4;
+                //PostMessage(h2, WM_RBUTTONDOWN, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                //SendMessage(h2, WM_PARENTNOTIFY, WM_RBUTTONDOWN, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+                //SendMessage(h2, WM_SETCURSOR, h3, (WM_RBUTTONDOWN<<16) |0x0001);
+            }
+
+        }
+
+        private void axWindowsMediaPlayer1_MouseMoveEvent(object sender, AxWMPLib._WMPOCXEvents_MouseMoveEvent e)
+        {
+
+            //Debug.WriteLine(e.nButton.ToString());
+            //Trace.Write(e.nButton.ToString());
+            //if (e.nButton != 0)
+            //{
+            //    switch (typ)
+            //    {
+            //        case 1:
+            //            PostMessage(h3, WM_LBUTTONUP, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //            break;
+            //        case 2:
+            //            PostMessage(h3, WM_RBUTTONUP, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //            break;
+            //        case 3:
+            //            PostMessage(h3, WM_LBUTTONDOWN, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //            break;
+            //        case 4:
+            //            PostMessage(h3, WM_RBUTTONDOWN, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //            break;
+            //    }
+            //    typ = -1;
+
+            //    if (e.nButton == 1)
+            //    {
+            //        // PostMessage(h2, WM_MOUSEMOVE, 1, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //        PostMessage(h3, WM_MOUSEMOVE, 1, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //    }
+            //    else if (e.nButton == 2)
+            //    {
+            //        //PostMessage(h2, WM_MOUSEMOVE, 2, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //        PostMessage(h3, WM_MOUSEMOVE, 2, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //    }
+            //    else
+            //    {
+            //        //PostMessage(h2, WM_MOUSEMOVE, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //        PostMessage(h3, WM_MOUSEMOVE, 0, (e.fX & 0xFFFF) + (e.fY & 0xFFFF) * 0x10000);
+            //    }
+            //}
+        }
+
+        private void 暂停ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (暂停ToolStripMenuItem.Text == "暂停")
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+                暂停ToolStripMenuItem.Text = "继续";
+            }
+            else
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                暂停ToolStripMenuItem.Text = "暂停";
+            }
+        }
+
+    }
+}
